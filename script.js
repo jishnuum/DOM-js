2e804b47890499e7851fde7ec1282e4a2b7c0821
jishnum <jishnuum2000@gmail.com>
2024-11-09 12:11:31 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let heading = document.getElementById("main-title")
+console.log(heading);
+
+let element = document.getElementsByClassName("container")
+console.log(element);
+
+let elements = document.getElementsByClassName("section")
+console.log(elements);
+
+let para = document.getElementsByTagName("p")
+console.log(para);
+
+let button = document.querySelector(".btn")
+console.log(button);
+
+let section = document.querySelector("section")
+console.log(section);
+
+let buttons = document.querySelectorAll(".btn")
+console.log(buttons);
+
+let paragraphs = document.querySelectorAll("p")
+console.log(paragraphs);
